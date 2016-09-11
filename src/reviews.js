50b8be423c313a4c70c23f985682407900a347d3
Andrey Karamnoff <karamnoff@gmail.com>
2016-09-11 10:46:46 +0300

    :sparkles: Use inheritance to extend Review class behaviour

--- src/reviews.js
+++ src/reviews.js
@@ -14,6 +14,8 @@ var reviewStorage = localStorage;
 var activeFilter = reviewStorage.getItem('activeFilter') || document.querySelector('input[type=radio][name=reviews][checked]').value;
 document.querySelector('input[type=radio][name=reviews][value=' + activeFilter + ']').checked = true;
 
+var reviewsInstances = [];
+
 var reviewsList = document.querySelector('.reviews-list');
 var reviewsFilter = document.querySelector('.reviews-filter');
 var reviewsControls = document.querySelector('.reviews-controls');
@@ -35,7 +37,14 @@ function drawReviews(data) {
   }
   var fragment = document.createDocumentFragment();
   reviews.forEach(function(reviewData) {
-    fragment.appendChild(new Review(reviewData).element);
+    // На каждой итерации по массиву reviews с данными, пришедшими с сервера:
+    // 1. В переменную reviewClass записывается экземпляр класса Review
+    // и добавляется в массив reviewsInstances;
+    // 2. В DOM-объект fragment добавляется DOM-элемент с отзывом,
+    // представленный св-вом element класса Review
+    var reviewClass = new Review(reviewData);
+    reviewsInstances.push(reviewClass);
+    fragment.appendChild(reviewClass.element);
   });
   reviewsList.appendChild(fragment);
 
@@ -48,7 +57,16 @@ function drawReviews(data) {
  * @param {String} filterID
  */
 function changeFilter(filterID) {
-  reviewsList.innerHTML = '';
+  // На каждой итерации по массиву reviewsInstances
+  // на элементе отзыва вызывается метод remove,
+  // удаляющий элемент отзыва из контейнера reviewsList
+  reviewsInstances.forEach(function(elem) {
+    elem.remove();
+  });
+  // Удаляем данные из массива,
+  // так как при повторном вызове функции drawReviews
+  // массив будет заполнен новыми данными
+  reviewsInstances = [];
   activeFilter = filterID;
   listIndex = 0;
   reviewsMore.classList.remove('invisible');
